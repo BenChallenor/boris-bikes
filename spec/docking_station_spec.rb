4bf710a73bd1bacd30a29c70a0a23bef970a92af
Matt Burstein <j.matthew.burstein@gmail.com>
2018-01-24 15:26:20 +0000

    Add DockingStation#dock() and @bikes variable to DockingStation

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,12 +1,19 @@
 require "docking_station"
 describe DockingStation do
+
   it "returns DockingStation to respond to the method release_bike" do
   expect(subject).to respond_to(:release_bike)
-end
+  end
+
   it "releases working bike" do
     docking_station = DockingStation.new
     expect(docking_station.release_bike).to be_working
+  end
 
-
-end
+  it "docks a bike" do
+    docking_station = DockingStation.new
+    bike = Bike.new
+    expect(docking_station.dock(bike)).to include bike
+  end
+  
 end
