e4590f7682772e4b9388ef2de917d2630576acc3
Ben <ben.challenor@btopenworld.com>
2018-01-24 14:11:50 +0000

    Ex 10

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,4 +3,10 @@ describe DockingStation do
   it "returns DockingStation to respond to the method release_bike" do
   expect(subject).to respond_to(:release_bike)
 end
+  it "releases working bike" do
+    docking_station = DockingStation.new
+    expect(docking_station.release_bike).to be_working
+
+
+end
 end
